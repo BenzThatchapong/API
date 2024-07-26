1a0da975820d6821a89dffcb1f589b0e3d9372d2
Thatchapong Tunkam <benzthatchapong@gmail.com>
2024-07-26 15:55:00 +0700

    first commit

--- src/flask-api.py
+++ src/flask-api.py
@@ -1,9 +1,11 @@
 # flask-app.py
 from flask import Flask, request
+from flask_cors import CORS
 import json
 
 # create a Flask instance
 app = Flask(__name__)
+CORS(app)
 
 # a simple description of the API written in html.
 # Flask can print and return raw text to the browser. 
@@ -55,3 +57,4 @@ if __name__ == "__main__":
 	
 	# for production
 	app.run(host='0.0.0.0', port=5000)
+ 
