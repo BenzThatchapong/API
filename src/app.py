48812c833701c01726f3e28f13befe0f503b6f58
Thatchapong Tunkam <benzthatchapong@gmail.com>
2024-07-26 15:30:11 +0700

    first commit

--- src/app.py
+++ src/app.py
@@ -1,17 +1,60 @@
-from flask import Flask,render_template
-import socket
+# flask-app.py
+from flask import Flask, request
+import json
 
+# create a Flask instance
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
-    except:
-        return render_template('error.html')
+# a simple description of the API written in html.
+# Flask can print and return raw text to the browser. 
+# This enables html, json, etc. 
 
+# description =   """
+#                 <!DOCTYPE html>
+#                 <head>
+#                 <title>API Landing</title>
+#                 </head>
+#                 <body>  
+#                     <h3>A simple API using Flask</h3>
+#                     <a href="http://localhost:5000/api?value=2">sample request</a>
+#                 </body>
+#                 """
+
+description = "Request Sussessful..."
+
+
+# Routes refer to url'
+# our root url '/' will show our html description
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    # return a html format string that is rendered in the browser
+	return description
+
+# our '/api' url
+# requires user integer argument: value
+# returns error message if wrong arguments are passed.
+@app.route('/api', methods=['POST','GET'])
+def square():
+    if not all(k in request.args for k in (["value"])):
+        # we can also print dynamically 
+        # using python f strings and with 
+        # html elements such as line breaks (<br>)
+        error_message =     f"\
+                            Required paremeters : 'value'<br>\
+                            Supplied paremeters : {[k for k in request.args]}\
+                            "
+        return error_message
+    else:
+        # assign and cast variable to int
+        value = int(request.args['value'])
+        # or use the built in get method and assign a type
+        # http://werkzeug.palletsprojects.com/en/0.15.x/datastructures/#werkzeug.datastructures.MultiDict.get
+        value = request.args.get('value', type=int)
+        return json.dumps({"Value Squared" : value**2})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+	# for debugging locally
+	# app.run(debug=True, host='0.0.0.0',port=5000)
+	
+	# for production
+	app.run(host='0.0.0.0', port=8080)
